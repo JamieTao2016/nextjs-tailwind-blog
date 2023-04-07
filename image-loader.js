0a37ae8d93c05176e8400c51c248d93929bead3f
JamieTao2016 <129752055+JamieTao2016@users.noreply.github.com>
2023-04-07 21:32:32 +0800

    Create image-loader.js

--- image-loader.js
+++ image-loader.js
@@ -1,3 +1,3 @@
-export default function myImageLoader({ src, width, quality }) {
+export default function mylmageLoader({ src, width, quality }) {
   return `/${src}?w=${width}&q=${quality || 75}`
 }
